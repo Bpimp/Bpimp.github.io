5051dbad7e0bf7d0ed4d6b8eea8de988e36df521
Bpimp <635733853@qq.com>
2019-04-12 13:03:02 +0800

    Updates

---
+++ precache-manifest.5d96e93c1cda1c780519dcb607a75ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d6d8e0b927418e2eff",
+    "url": "/note/static/js/runtime~main.2b220f5a.js"
+  },
+  {
+    "revision": "565bc373d06f8141ff50",
+    "url": "/note/static/js/main.e61e000a.chunk.js"
+  },
+  {
+    "revision": "096952bc725f3eebb191",
+    "url": "/note/static/js/2.0f529f8d.chunk.js"
+  },
+  {
+    "revision": "565bc373d06f8141ff50",
+    "url": "/note/static/css/main.ba022078.chunk.css"
+  },
+  {
+    "revision": "a8c9d7ea2669b61869bced060630feab",
+    "url": "/note/index.html"
+  }
+];
